9381763ce4f15412b9b7099edf0c4985235a96e6
RichardG867 <richardg867@gmail.com>
2026-04-16 19:17:49 -0300

    Disable PDF duplex mode

--- conf.py
+++ conf.py
@@ -75,6 +75,7 @@ html_favicon = 'favicon.ico'
 
 latex_toplevel_sectioning = 'section'
 latex_elements = {
+	'extraclassoptions': 'openany,oneside',
 	'preamble': r'''
 \addto\captionsenglish{\renewcommand{\contentsname}{Contents}}
 '''
